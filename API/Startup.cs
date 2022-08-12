04dd45600dfab58ef64b97d5bddac33877566c34
riay2100 <riay2100@student.miun.se>
2022-08-12 21:51:48 +0200

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,11 +38,19 @@ namespace API
 
             services.AddDbContext<DataContext>(opt =>
             {
-               opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
 
-    
-            
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +66,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
